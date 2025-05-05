6273a20d39a61ce8ce0e5ba7987dbb91be93a013
ajaygittest <yesurajadgl@gmail.com>
2025-05-05 20:42:55 +0530

    Maximum Consecutive Number

---
+++ Java/MaxiumConsecutive.java
@@ -0,0 +1,27 @@
+package Practice;
+
+public class MaxiumConsecutive {
+	
+	public static void main(String[] args) {
+		int[] arr={1, 1, 0, 1, 1};
+		System.out.println(consecutive(arr));
+	}
+	
+	public static int consecutive(int[]arr) {
+		
+		int max=0;
+		int count=0;
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			if(arr[i]==1) {
+				count++;
+				max=Math.max(max, count);
+			}else {
+				count=0;
+			}
+		}
+		return max;
+	}
+
+}
